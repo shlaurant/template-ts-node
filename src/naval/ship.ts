82cd87f121912efc3f85d5b50bccc06284f36393
Laurent <laurent@laurentui-MacBookPro.local>
2025-02-11 15:47:12 +0900

    ship

--- src/naval/ship.ts
+++ src/naval/ship.ts
@@ -1,1 +1,18 @@
-export type Ship = Readonly<{}>
+export type Reactor = Readonly<{
+  power: number;
+}>
+
+export type Machinery = Readonly<{
+  powerConsumption: number
+}>
+
+export type Gun = Readonly<{
+  powerConsumption: number
+}>
+
+export type Ship = Readonly<{
+  displacement: number
+  reactor: Reactor
+  machinery: Machinery
+  primaryGuns: ReadonlyArray<Gun>
+}>
