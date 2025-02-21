2bb53d03864b29624e48f6dd7ae635e80b8e7200
Laurent <laurent@laurentui-MacBookPro.local>
2025-02-21 15:49:03 +0900

    Manor.produce

---
+++ src/temp/region.ts
@@ -0,0 +1,24 @@
+export type Good = "Wheat" | "Wood"
+
+export type Field = Readonly<{
+  name: string
+  outputs: Map<Good, number>
+}>
+
+export type Manor = Readonly<{
+  size: number;
+  fields: ReadonlyArray<Field>
+  reserves: Map<Good, number>
+}>
+
+export function produce(manor: Manor): Manor {
+  const newReserves = manor.reserves
+
+  manor.fields.forEach(e => {
+    e.outputs.forEach((v, k) => {
+      newReserves.set(k, v + (manor.reserves.get(k) ?? 0))
+    })
+  })
+
+  return { ...manor, reserves: newReserves }
+}
