2a3fd388dc9891027659757c2bd5ecc2261df308
Laurent <laurent@laurentui-MacBookPro.local>
2025-03-31 18:39:52 +0900

    refactor using type

--- src/naval/main.ts
+++ src/naval/main.ts
@@ -4,62 +4,50 @@ function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-class Ship {
-  #hp: number
-  #armor: number
-  #dmg: number
-  #penetration: number
+type Ship = {
+  hp: number
+  armor: number
+  dmg: number
+  penetration: number
+}
 
-
-  constructor(hp: number, armor: number, dmg: number, penetration: number) {
-    this.#hp = hp
-    this.#armor = armor
-    this.#dmg = dmg
-    this.#penetration = penetration
-  }
-
-  shoot(other: Ship): void {
-    if (this.#penetration > other.#armor) {
-      other.#hp = Math.max(0, other.#hp - this.#dmg)
-    }
-  }
-
-  isAlive(): boolean {
-    return this.#hp > 0
+function shoot(from: Ship, other: Ship): void {
+  if (from.penetration > other.armor) {
+    other.hp = Math.max(0, other.hp - from.dmg)
   }
 }
 
-class Division {
-  #ships: Ship[]
+function isAlive(s: Ship): boolean {
+  return s.hp > 0
+}
 
-  constructor(ships: Ship[]) {
-    this.#ships = ships
-  }
+type Division = {
+  ships: Ship[]
+}
 
-  isOver(): boolean {
-    return this.#ships.every(ship => !ship.isAlive())
-  }
+function isOver(div: Division): boolean {
+  return div.ships.every(ship => !isAlive(ship))
+}
 
-  aliveShips(): Ship[] {
-    return this.#ships.filter(ship => ship.isAlive())
-  }
+function shipsAlive(div: Division): Ship[] {
+  return div.ships.filter(ship => isAlive(ship))
+}
 
-  getRandomAlive(): Ship {
-    while (true) {
-      const ret = this.#ships[Math.floor(Math.random() * this.#ships.length)]
-      if (ret.isAlive()) {
-        return ret
-      }
+function getRandomAlive(div: Division): Ship {
+  while (true) {
+    const ret = div.ships[Math.floor(Math.random() * div.ships.length)]
+    if (isAlive(ret)) {
+      return ret
     }
   }
+}
 
-  getRandomsAlive(n: number): Ship[] {
-    const ret = []
-    for (let i = 0; i < n; i++) {
-      ret.push(this.getRandomAlive())
-    }
-    return ret
+function getRandomsAlive(div: Division, n: number): Ship[] {
+  const ret = []
+  for (let i = 0; i < n; i++) {
+    ret.push(getRandomAlive(div))
   }
+  return ret
 }
 
 enum RelativeDirection {
@@ -72,34 +60,34 @@ enum RelativeDirection {
 }
 
 function defaultShip(): Ship {
-  return new Ship(100, 5, 10, 10)
+  return { armor: 5, dmg: 10, hp: 100, penetration: 10 }
 }
 
 function main() {
-  const div1 = new Division([defaultShip(), defaultShip()])
-  const div2 = new Division([defaultShip(), defaultShip()])
+  const div1: Division = { ships: [defaultShip(), defaultShip()] }
+  const div2: Division = { ships: [defaultShip(), defaultShip()] }
 
-  while (!div1.isOver() && !div2.isOver()) {
-    const s1 = div1.aliveShips()
-    const s2 = div2.aliveShips()
+  while (!isOver(div1) && !isOver(div2)) {
+    const s1 = shipsAlive(div1)
+    const s2 = shipsAlive(div2)
 
-    const t1 = div2.getRandomsAlive(s1.length)
-    const t2 = div1.getRandomsAlive(s2.length)
+    const t1 = getRandomsAlive(div2, s1.length)
+    const t2 = getRandomsAlive(div1, s2.length)
 
     for (let i = 0; i < s1.length; i++) {
-      s1[i].shoot(t1[i])
+      shoot(s1[i], t1[i])
     }
 
     for (let i = 0; i < s2.length; i++) {
-      s2[i].shoot(t2[i])
+      shoot(s2[i], t2[i])
     }
   }
 
-  if (div1.isOver() && div2.isOver()) {
+  if (isOver(div1) && isOver(div1)) {
     console.log("draw")
-  } else if (div1.isOver()) {
+  } else if (isOver(div1)) {
     console.log("div2 won")
-  } else if (div2.isOver()) {
+  } else if (isOver(div2)) {
     console.log("div1 won")
   }
 }
