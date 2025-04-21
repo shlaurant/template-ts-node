f1ffbbdfbb3c191bfd11c30f632c8908682bd83e
Laurent <laurent@laurentui-MacBookPro.local>
2025-04-21 22:35:52 +0900

    wip

---
+++ src/battle/main.ts
@@ -0,0 +1,36 @@
+type Slot = {
+  y: number
+  x: number
+}
+
+type Board<T extends Slot> = T[][]
+
+function createBoard<T extends Slot>(y: number, x: number, value: T): Board<T> {
+  const ret: T[][] = []
+
+  for (let i = 0; i < y; ++i) {
+    const rw: T[] = []
+    for (let j = 0; j < x; ++j) {
+      rw.push(value)
+    }
+    ret.push(rw)
+  }
+
+  return ret
+}
+
+type Character = {
+  id: number
+  hp: number
+}
+
+const NullCharacter = {
+  id: -1,
+  hp: -1
+}
+
+type CharacterSlot = Slot & { character: Character }
+
+function main() {
+
+}
