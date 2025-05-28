60b46204f64a14b926ab470975a52e12a0f81da2
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-28 23:59:47 +0900

    assignToQuest

--- src/naval/ship.ts
+++ src/naval/ship.ts
@@ -1,4 +1,17 @@
+import { Id, Identifiable } from "./id"
+
 export type Ship = Readonly<{
   combat: number,
   upkeep: number
 }>
+
+export type QuestAssignment = Readonly<{
+  questId: Id
+}>
+
+function assignToQuest<T extends object>(ship: Ship, quest: Identifiable<T>): Ship & QuestAssignment {
+  return {
+    ...ship,
+    questId: quest.id
+  }
+}
