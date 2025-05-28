f1eb964ff418b7be6875ac603b5f3e324e1f86d3
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-28 23:49:03 +0900

    AssignShipsToQuest

--- src/naval/quest.ts
+++ src/naval/quest.ts
@@ -1,8 +1,23 @@
+export type Id = number
+
 export type Quest = Readonly<{
   difficulty: number
   length: number
   reward: number
 }>
+
+export type ShipAssignment = Readonly<{
+  assignedAt: number
+  shipIds: ReadonlyArray<Id>
+}>
+
+export function AssignShipsToQuest(turn: number, quest: Quest, shipIds: ReadonlyArray<Id>): Quest & ShipAssignment {
+  return {
+    ...quest,
+    assignedAt: turn,
+    shipIds: shipIds
+  }
+}
 
 export const Quests: Quest[] = [
   { difficulty: 1, length: 1, reward: 1 },
@@ -12,5 +27,5 @@ export const Quests: Quest[] = [
   { difficulty: 1, length: 3, reward: 3 },
   { difficulty: 4, length: 1, reward: 4 },
   { difficulty: 2, length: 2, reward: 4 },
-  { difficulty: 1, length: 4, reward: 4 },
+  { difficulty: 1, length: 4, reward: 4 }
 ]
