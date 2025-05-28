2a651ec9cf3140de6fed66f1ab2ed0de5902da67
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-29 00:32:43 +0900

    helper function id()

--- src/naval/id.ts
+++ src/naval/id.ts
@@ -1,7 +1,30 @@
+
 export type Id = number
 
 export type Identifiable<T extends object> = T & {
   id: Id
+}
+
+export function id(src: unknown): Id {
+  let ret: Id | undefined;
+
+  if(typeof src === 'string') {
+    try {
+      src = Number(src)
+    } catch {
+      throw new Error(`invalid id: ${src}`)
+    }
+  }
+
+  if(typeof src === 'number' && Number.isInteger(src)) {
+    ret = src
+  }
+
+  if(ret === undefined) {
+    throw new Error(`invalid id: ${src}`)
+  }
+
+  return ret
 }
 
 export function giveId<T extends object>(obj: T): Identifiable<T> {
