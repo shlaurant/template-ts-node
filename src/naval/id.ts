94a2d76efbb7739231338f481265370124724ea3
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-28 23:55:26 +0900

    give Id

--- src/naval/id.ts
+++ src/naval/id.ts
@@ -1,1 +1,12 @@
 export type Id = number
+
+export type Identifiable<T extends object> = T & {
+  id: Id
+}
+
+export function giveId<T extends object>(obj: T): Identifiable<T> {
+  return {
+    ...obj,
+    id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+  }
+}
