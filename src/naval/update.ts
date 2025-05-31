61f8128b74b30c4593628960af2b6c951f18747c
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-31 15:08:10 +0900

    update Dispatch status

---
+++ src/naval/update.ts
@@ -0,0 +1,43 @@
+import { Id, Identifiable } from "./id"
+import { dismissQuest, Quest, QuestAssignment } from "./quest"
+import { dismissShips, Ship, ShipAssignment } from "./ship"
+
+export type UpdateDispatchResult = {
+  quests: ReadonlyMap<Id, Identifiable<Quest>>
+  ships: ReadonlyMap<Id, Identifiable<Ship>>
+  rewards: number
+  events: string[]
+}
+
+export function updateDispatchStatus(
+  turn: number,
+  quests: ReadonlyMap<Id, Identifiable<Quest & ShipAssignment>>,
+  ships: ReadonlyMap<Id, Identifiable<Ship & QuestAssignment>>,
+): UpdateDispatchResult {
+  const ret = {
+    quests: new Map<Id, Identifiable<Quest>>(),
+    ships: new Map<Id, Identifiable<Ship>>(),
+    rewards: 0,
+    events: [] as string[]
+  }
+
+  const toDismiss = new Set<Id>
+
+  quests.forEach((v, k)=>{
+    if(v.assignedAt + v.length > turn) {
+      ret.quests.set(k, dismissShips(v))
+      ret.rewards += v.reward
+      v.shipIds.forEach(toDismiss.add)
+    }
+  })
+
+  ships.forEach((v, k)=>{
+    if(toDismiss.has(k)) {
+      ret.ships.set(k, dismissQuest(v))
+    } else {
+      ret.ships.set(k, v)
+    }
+  })
+
+  return ret
+}
