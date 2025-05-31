064de2e2b005512a2e4040bc1002d991b7f1ce9b
Laurent <laurent@laurentui-MacBookPro.local>
2025-05-31 16:38:05 +0900

    test

---
+++ src/naval/update.test.ts
@@ -0,0 +1,21 @@
+import { Identifiable } from "./id"
+import { Quest, QuestAssignment } from "./quest"
+import { Ship, ShipAssignment } from "./ship"
+import { updateDispatchStatus } from "./update"
+
+test("updateDispatchStatus", () => {
+  const turn = 2
+  const quests: ReadonlyArray<Identifiable<Quest & ShipAssignment>> = [
+    { assignedAt: 0, difficulty: 0, id: 0, length: 1, reward: 1, shipIds: [1] },
+  ]
+  const ships: ReadonlyArray<Identifiable<Ship & QuestAssignment>> = [
+    { combat: 0, id: 1, questId: 0, upkeep: 0 },
+  ]
+
+  const ret = updateDispatchStatus(turn, quests, ships)
+
+  expect(ret.quests).toEqual([{ difficulty: 0, id: 0, length: 1, reward: 1 }])
+  expect(ret.ships).toEqual([{ combat: 0, id: 1, upkeep: 0 }])
+  expect(ret.rewards).toBe(1)
+  expect(ret.events).toHaveLength(1)
+})
