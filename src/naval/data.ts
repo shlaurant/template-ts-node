9d42bc3305baf53d7f9af1c31fbe87c838cdd62d
Laurent <laurent@laurentui-MacBookPro.local>
2025-06-01 15:47:58 +0900

    handle event

--- src/naval/data.ts
+++ src/naval/data.ts
@@ -4,6 +4,7 @@ import { Ship } from "./core/ship"
 import { Quest } from "./core/quest"
 import { DispatchShipsReturn } from "./core/command"
 import { UpdateDispatchResult } from "./core/turn"
+import { EventString, isEvent } from "./core/event"
 
 export type Data = {
   turn: number
@@ -12,11 +13,13 @@ export type Data = {
   balance: number
   ships: Map<Id, Identifiable<Ship>>
   quests: Map<Id, Identifiable<Quest>>
+  events: EventString[]
 }
 
 export function updateDispatchShipsReturn(data: Data, value: DispatchShipsReturn): Data {
   data.quests.set(value.quest.id, value.quest)
   value.ships.forEach((e) => data.ships.set(e.id, e))
+  handleEvent(data, value)
   return data
 }
 
@@ -24,5 +27,12 @@ export function updateCheckDispatchReturn(data: Data, value: UpdateDispatchResul
   value.quests.forEach((e) => data.quests.set(e.id, e))
   value.ships.forEach((e) => data.ships.set(e.id, e))
   data.balance += value.rewards
+  handleEvent(data, value)
   return data
+}
+
+function handleEvent<T extends object>(data:Data, value: T) {
+  if(isEvent(data)) {
+    data.events.push(data.event)
+  }
 }
