641476301a47a3935552728df50764ccd5ef2e88
Laurent <laurent@laurentui-MacBookPro.local>
2025-06-01 15:07:14 +0900

    UpdateDispatchShipsReturn

--- src/naval/data.ts
+++ src/naval/data.ts
@@ -1,6 +1,7 @@
 import { Id, Identifiable } from "./core/id"
 import { Ship } from "./core/ship"
 import { Quest } from "./core/quest"
+import { DispatchShipsReturn } from "./core/command"
 
 export type Data = {
   turn: number
@@ -9,4 +10,9 @@ export type Data = {
   balance: number
   ships: Map<Id, Identifiable<Ship>>
   quests: Map<Id, Identifiable<Quest>>
+}
+
+export function UpdateDispatchShipsReturn(data: Data, value: DispatchShipsReturn) {
+  data.quests.set(value.quest.id, value.quest)
+  value.ships.forEach((e) => data.ships.set(e.id, e))
 }
