f82484753a1b760004f4302ca8f3282b50d1589d
Laurent <laurent@laurentui-MacBookPro.local>
2025-06-03 15:48:59 +0900

    gaussianRandom

---
+++ src/random/normal-distribution.ts
@@ -0,0 +1,10 @@
+function gaussianRandom(mean: number = 0, stdev: number = 1) {
+  let u = 1
+  while (!(u < 1)) {
+    u = 1 - Math.random()
+  }
+  const v = Math.random()
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+  // Transform to the desired mean and standard deviation:
+  return z * stdev + mean
+}
